d93556c423823a39edc7f9adb8a73bde7b9548a3
Bruno Waiego <45051790+Bruno-Waiego@users.noreply.github.com>
2019-11-17 14:19:11 -0300

    Add files via upload

---
+++ src/test/java/br/com/animais/adocao/dao/teste/CadastroOngTeste.java
@@ -0,0 +1,39 @@
+package br.com.animais.adocao.dao.teste;
+
+import org.junit.jupiter.api.Test;
+
+import br.com.animais.adocao.model.Endereco;
+import br.com.animais.adocao.model.Ong;
+import br.com.animais.adocao.model.Usuario;
+
+class CadastroOngTeste {
+
+	private Ong ong;
+
+	@Test
+	void testCadastroOng() {
+		ong = new Ong();
+		Usuario usu = new Usuario();
+		usu.setLogin("@gmail");
+		usu.setSenha("senha");
+
+		Endereco end = new Endereco();
+		end.setLogradouro("log");
+		end.setNumero("123");
+		end.setBairro("Barra");
+		end.setCep("89212-128");
+		end.setCidade("Floria");
+		end.setEstado("SC");
+
+		ong.setUsuario(usu);
+		ong.setEndereco(end);
+
+		ong.setNome("Ação animal");
+		ong.setTelefone("(99) 99999-9999");
+		ong.setEmail("@gmail");
+		ong.setDescricao("Animais de rua");
+		ong.setCnpj("99.999.999/9999-99");
+
+		System.out.println("Ong:" + ong.getNome());
+	}
+}
